74577f29903b3b71b9a99302840fee4ec2b19fc5
GoldenStack <goldenfire64yt@gmail.com>
2024-07-10 00:30:52 -0500

    Parse queries from subdomain

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use axum::{
     body::Body, extract::{Host, Request}, response::Html, routing::get, Router
 };
 use anyhow::Result;
+use thiserror::Error;
 
 // request types:
 
@@ -27,6 +28,41 @@ use anyhow::Result;
 // for some reason web frameworks support placeholders in example.com/PLACEHOLDER
 // but not PLACEHOLDER.example.com. i wonder why.
 
+#[derive(Debug)]
+enum Query<'a> {
+    Login { username: &'a str, password: &'a str },
+    Logout { username: &'a str, password: &'a str },
+    Register { username: &'a str, password: &'a str },
+    Get { username: &'a str },
+    Set { x: u8, y: u8, checked: bool, username: &'a str, password: &'a str }
+}
+
+#[derive(Error, Debug)]
+enum CatastrophicFailure {
+    #[error("Bad hostname. Please try again never.")]
+    BadHostname,
+
+    #[error("No subdomain.")]
+    NoSubdomain,
+
+    #[error("Unknown subdomain query {0}")]
+    UnknownSubdomainQuery(String),
+
+    #[error("Invalid length of query.")]
+    InvalidQueryLength,
+
+    #[error("Expected text {text} at position {position}.")]
+    ExpectedQueryText {
+        text: String,
+        position: usize
+    },
+
+    #[error("Illegal character in query.")]
+    IllegalCharacter
+}
+
+const HOSTNAME: &str = "secure-website.localhost:1472";
+
 #[tokio::main]
 async fn main() -> Result<()> {
     // no need for multiple handlers because of the incrediblly good design of our app
@@ -37,15 +73,126 @@ async fn main() -> Result<()> {
         .await
         .unwrap();
 
+    // help me
     println!("ufortuately we are listening on {}", listener.local_addr().unwrap());
     axum::serve(listener, app).await.unwrap();
 
     Ok(())
 }
 
-// we can just completely ignore CORS because HEAD requests give us all the information we need
+// sidenote: we can just completely ignore CORS because HEAD requests give us all the information we need
 
 async fn handler(Host(hostname): Host, request: Request<Body>) -> Html<String> {
     println!("giga nerd request @ {}", hostname);
-    Html(format!("<h1>{}</h1>", hostname))
+
+    let query = match parse_host(&hostname).and_then(parse_query) {
+        Ok(q) => q,
+        Err(e) => return Html(format!("Error: {:?}", e))
+    };
+
+    println!("parsed: {:?}", query);
+
+    Html(format!("<h1>{:?}</h1>", query))
+}
+
+fn parse_host<'a>(host: &'a str) -> Result<&'a str> {
+
+    // Parse out hostname
+    if !host.ends_with(HOSTNAME) {
+        return Err(CatastrophicFailure::BadHostname.into());
+    }
+
+    let host = &host[0..host.len()-HOSTNAME.len()];
+
+    if !host.ends_with(".") {
+        return Err(CatastrophicFailure::NoSubdomain.into());
+    }
+
+    let mut host = &host[0..host.len()-1];
+
+    if let Some(index) = host.rfind(".") {
+        host = &host[index+1..];
+    }
+
+    if !host.chars().all(|c| c.is_ascii_alphanumeric() || c == '-') {
+        return Err(CatastrophicFailure::IllegalCharacter.into())
+    }
+
+    Ok(host)
+}
+
+fn parse_query<'a>(string: &'a str) -> Result<Query<'a>> {
+
+    // let me know if i can not allocate here somehow
+    let sections = string.split("-").collect::<Vec<_>>();
+
+    let Some(first) = sections.get(0) else {
+        return Err(CatastrophicFailure::InvalidQueryLength.into());
+    };
+
+    match *first {
+        "login" => {
+            if let Err(e) = require_params(&sections, ["login", "username", "_", "password", "_"]) {
+                return Err(e);
+            }
+            Ok(Query::Login { username: sections[2], password: sections[4] })
+        }
+        "logout" => {
+            if let Err(e) = require_params(&sections, ["logout", "username", "_", "password", "_"]) {
+                return Err(e);
+            }
+            Ok(Query::Logout { username: sections[2], password: sections[4] })
+        }
+        "register" => {
+            if let Err(e) = require_params(&sections, ["register", "username", "_", "password", "_"]) {
+                return Err(e);
+            }
+            Ok(Query::Register { username: sections[2], password: sections[4] })
+        }
+        "get" => {
+            if let Err(e) = require_params(&sections, ["get", "username", "_"]) {
+                return Err(e);
+            }
+            Ok(Query::Get { username: sections[2] })
+        }
+        "set" => {
+            if let Err(e) = require_params(&sections, ["set", "checkbox", "x", "_", "y", "_", "to", "_", "username", "_", "password", "_"]) {
+                return Err(e);
+            }
+
+            let Ok(x) = sections[3].parse() else {
+                return Err(CatastrophicFailure::IllegalCharacter.into());
+            };
+
+            let Ok(y) = sections[5].parse() else {
+                return Err(CatastrophicFailure::IllegalCharacter.into());
+            };
+
+            let checked = match sections[7] {
+                "checked" => true,
+                "unchecked" => false,
+                _ => return Err(CatastrophicFailure::IllegalCharacter.into())
+            };
+
+            Ok(Query::Set { x, y, checked, username: sections[9], password: sections[11] })
+        }
+        _ => Err(CatastrophicFailure::UnknownSubdomainQuery(first.to_string()).into())
+    }
+}
+
+fn require_params<const N: usize>(query: &Vec<&str>, params: [&str; N]) -> Result<()> {
+    
+    if query.len() != params.len() {
+        return Err(CatastrophicFailure::InvalidQueryLength.into());
+    }
+
+    for (index, str) in params.iter().enumerate() {
+        if str == &"_" { continue; }
+
+        if &query[index] != str {
+            return Err(CatastrophicFailure::ExpectedQueryText { text: str.to_string(), position: index }.into())
+        }
+    }
+
+    Ok(())
 }
